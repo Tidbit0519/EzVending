198cb549d90a9ad6388bd0a6cc5b33a948ba8a62
Jason Ban Tze Tan <jtbt1218@gmail.com>
2022-08-30 10:44:46 -1000

    Added sql file create table

---
+++ FAVORITE_SNACKS.sql
@@ -0,0 +1,19 @@
+--------------------------------------------------------
+--  File created - Tuesday-August-30-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table FAVORITE_SNACKS
+--------------------------------------------------------
+
+  CREATE TABLE "OIT#JTBT2"."FAVORITE_SNACKS" 
+   (	"BYU_ID" VARCHAR2(9 BYTE), 
+	"FULL_NAME" VARCHAR2(30 BYTE), 
+	"SNACKS" VARCHAR2(30 BYTE), 
+	"PRICE" VARCHAR2(5 BYTE), 
+	"PURCHASE_DATE" VARCHAR2(10 BYTE)
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+REM INSERTING into OIT#JTBT2.FAVORITE_SNACKS
+SET DEFINE OFF;
